219635aefc4c8b6a7ec6749e3cb99dad529d3be8
AdamAtGit <43450212+AdamAtGit@users.noreply.github.com>
2019-03-18 18:51:58 -0400

    Before adding sqlite.cs files, and the sqlite-net. 1.0.8 folder in package folder, and adding the dbPath and create table code in App.cs file.

---
+++ PHRApp/CL_UWP/SpeechClasses/PHRMediaElementExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace CL_UWP.SpeechClasses
+{
+    public static class PHRMediaElementExtensions
+    {
+
+        public static async Task PrompterPlayStreamAsync(
+      //? this MediaElement mediaElement,
+      this MediaElement mediaElement,
+      IRandomAccessStream stream,
+      bool disposeStream = true)
+        {
+            // bool is irrelevant here, just using this to flag task completion.
+            TaskCompletionSource<bool> taskCompleted = new TaskCompletionSource<bool>();
+
+            // Note that the MediaElement needs to be in the UI tree for events
+            // like MediaEnded to fire.
+            RoutedEventHandler endOfPlayHandler = (s, e) =>
+            {
+                if (disposeStream)
+                {
+                    stream.Dispose();
+                }
+                taskCompleted.SetResult(true);
+            };
+
+            mediaElement.MediaEnded += endOfPlayHandler;
+
+            mediaElement.SetSource(stream, string.Empty);
+            mediaElement.Play();
+
+            await taskCompleted.Task;
+
+            mediaElement.MediaEnded -= endOfPlayHandler;
+        }
+}
